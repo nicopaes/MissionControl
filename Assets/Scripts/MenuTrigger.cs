bc47c8cd7de374c5f6b0adfa13a5c72e4284a804
Trigs <mtrigsauer@gmail.com>
2018-05-06 12:27:29 -0300

    Basic menu transitions and Screens

---
+++ Assets/Scripts/MenuTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuTrigger : MonoBehaviour {
+
+	public void SpaceStation () 
+	{
+		SceneManager.LoadScene ("SpaceStation");
+		Debug.Log ("ACCESS GRANTED");
+	}
+
+	public void CreditScreen () 
+	{
+		SceneManager.LoadScene ("Credits");
+		Debug.Log ("CREW MEMBERS");
+	}
+
+	public void Exit () 
+	{
+		Application.Quit();
+	}
+
+	public void MainMenu () 
+	{
+		SceneManager.LoadScene ("MainMenu");
+		Debug.Log ("RETURN");
+	}
+
+	public void StartGame () 
+	{
+		SceneManager.LoadScene ("MovementTest");
+		Debug.Log ("LAUNCHING");
+	}
+
+}
